3fdb85de71ff4bd4f64f91fdde92b61bbc74bbe5
zerom-H <cupdisin@gmail.com>
2022-10-21 20:19:19 +0900

    EnableWebSecurity

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -3,13 +3,16 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) {
-
-        return null;
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.formLogin();
+        return http.build();
     }
 }
